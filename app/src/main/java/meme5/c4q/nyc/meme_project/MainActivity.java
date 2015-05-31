2c288cacc67c8ceea9cab97fc1c4abaa5e2b0256
Pooja Pasawala <pooja@basicer.com>
2015-05-31 16:30:57 -0400

    WIP blerg

--- app/src/main/java/meme5/c4q/nyc/meme_project/MainActivity.java
+++ app/src/main/java/meme5/c4q/nyc/meme_project/MainActivity.java
@@ -6,9 +6,13 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.graphics.BitmapFactory;
+import android.widget.Toast;
 import android.view.View;
 
 public class MainActivity extends Activity {
+    private int RESULT_LOAD_IMG = 1;
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private String imgFilePath;
@@ -33,6 +37,11 @@ public class MainActivity extends Activity {
         }
     }
 
+    public void loadImagefromGallery(View view) {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
@@ -51,7 +60,45 @@ public class MainActivity extends Activity {
             cursor.close();
             launchChooseMeme();
         }
+
     }
+
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        try {
+//            // When an Image is picked
+//            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+//                    && null != data) {
+//                // Get the Image from data
+//
+//                Uri selectedImage = data.getData();
+//                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+//
+//                // Get the cursor
+//                Cursor cursor = getContentResolver().query(selectedImage,
+//                        filePathColumn, null, null, null);
+//                // Move to first row
+//                cursor.moveToFirst();
+//
+//                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+//                String imgDecodableString = cursor.getString(columnIndex);
+//                cursor.close();
+//                ImageView imgView = (ImageView) findViewById(R.id.imgView);
+//
+//                // Set the Image in ImageView after decoding the String
+//                imgView.setImageBitmap(BitmapFactory
+//                        .decodeFile(imgDecodableString));
+//
+//            } else {
+//                Toast.makeText(this, "You haven't picked Image",
+//                        Toast.LENGTH_LONG).show();
+//            }
+//        } catch (Exception e) {
+//            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+//                    .show();
+//        }
+//
+//    }
 
 }
 
