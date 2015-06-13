271a833ce21084ed3d3fe64a8d240b0d36cc17e5
RosmaryFC <Rosmaryfc@hotmail.com>
2015-06-12 20:03:41 -0400

    refactored code in ChooseMemeStyle

--- app/src/main/java/meme5/c4q/nyc/meme_project/ChooseMemeStyle.java
+++ app/src/main/java/meme5/c4q/nyc/meme_project/ChooseMemeStyle.java
@@ -8,28 +8,16 @@ import android.graphics.Typeface;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.Random;
 
 
 public class ChooseMemeStyle extends Activity {
 
-
     public static final String TAG = "MEME_ACTIVITY";
-    protected RadioGroup styleGroup;
-    protected RadioButton styleButton;
-    protected Button nextButton;
     protected boolean vanilla;
-    protected Random slideShow;
     ImageView img, thumbnail;
     AnimationDrawable frameAnimation;
     String imgFilePath;
@@ -40,6 +28,17 @@ public class ChooseMemeStyle extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_meme_style);
 
+        getMeme();
+
+        applyCustomFont();
+
+        RadioGroup group = (RadioGroup) findViewById(R.id.styleGroup);
+        group.setOnCheckedChangeListener(radioGroupChangeListener);
+        group.check(R.id.chooseDemotivational);
+
+    }
+
+    public void getMeme() {
         Bundle bundle = getIntent().getExtras();
         if(bundle.getString("imgFilePath") != null)
         {
@@ -48,7 +47,9 @@ public class ChooseMemeStyle extends Activity {
         thumbnail = (ImageView) findViewById(R.id.thumbnail);
         Bitmap bmp2 = BitmapFactory.decodeFile(imgFilePath);
         thumbnail.setImageBitmap(bmp2);
+    }
 
+    public void applyCustomFont(){
         //apply fonts -
         //TODO: need to find a way to add this font globally through style!
         TextView step1 = (TextView) findViewById(R.id.step1);
@@ -65,64 +66,59 @@ public class ChooseMemeStyle extends Activity {
         title.setTypeface(tf);
         vanillaRadio.setTypeface(tf);
         demotivationalRadio.setTypeface(tf);
+    }
 
-        RadioGroup group = (RadioGroup) findViewById(R.id.styleGroup);
-        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
-                Log.d(TAG, "onCheckedChanged()");
-                // Is the button now checked?
+    public RadioGroup.OnCheckedChangeListener radioGroupChangeListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+            Log.d(TAG, "onCheckedChanged()");
+            // Is the button now checked?
 //                boolean checked = ((RadioButton) view).isChecked();
 
-                // Check which radio button was clicked
-                switch (checkedId) {
-                    case R.id.chooseVanilla:
+            // Check which radio button was clicked
+            switch (checkedId) {
+                case R.id.chooseVanilla:
 
-                        Log.d(TAG, "onCheckedChanged() -- chooseVanilla");
-                        //determine if user chose vanilla or demotivational
+                    Log.d(TAG, "onCheckedChanged() -- chooseVanilla");
+                    //determine if user chose vanilla or demotivational
 
-                        vanilla = true;
-                        img = (ImageView) findViewById(R.id.sampleImageHolder);
-                        img.setImageResource(R.drawable.vanilla_animation);
+                    vanilla = true;
+                    img = (ImageView) findViewById(R.id.sampleImageHolder);
+                    img.setImageResource(R.drawable.vanilla_animation);
 
-                        frameAnimation = (AnimationDrawable) img.getDrawable();
+                    frameAnimation = (AnimationDrawable) img.getDrawable();
 
-                        frameAnimation.start();
-                        break;
-                    case R.id.chooseDemotivational:
+                    frameAnimation.start();
+                    break;
+                case R.id.chooseDemotivational:
 
-                        Log.d(TAG, "onCheckedChanged() -- chooseDemotivational");
-                        //determine if user chose vanilla or demotivational
-                        vanilla = false;
-                        img = (ImageView) findViewById(R.id.sampleImageHolder);
-                        img.setImageResource(R.drawable.demotivational_animation);
+                    Log.d(TAG, "onCheckedChanged() -- chooseDemotivational");
+                    //determine if user chose vanilla or demotivational
+                    vanilla = false;
+                    img = (ImageView) findViewById(R.id.sampleImageHolder);
+                    img.setImageResource(R.drawable.demotivational_animation);
 
-                        frameAnimation = (AnimationDrawable) img.getDrawable();
+                    frameAnimation = (AnimationDrawable) img.getDrawable();
 
-                        frameAnimation.start();
-                        break;
-                }
+                    frameAnimation.start();
+                    break;
             }
-        });
+        }
+    };
 
-        group.check(R.id.chooseDemotivational);
-
-        nextButton = (Button) findViewById(R.id.nextButton);
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                if (vanilla) {
-                    Intent vanillameme = new Intent(ChooseMemeStyle.this,VanillaMeme.class);
-                    vanillameme.putExtra("imgFilePath",imgFilePath);
-                    startActivity(vanillameme);
-                }
-                else {
-                    Intent demotivationalMeme = new Intent(ChooseMemeStyle.this, DemotivationalMemeActivity.class);
-                    demotivationalMeme.putExtra("imgFilePath",imgFilePath);
-                    startActivity(demotivationalMeme);
-                }
-            }
-        });
+    public void nextButtonOnClick (View view) {
+        if (vanilla) {
+            Intent vanillameme = new Intent(ChooseMemeStyle.this,VanillaMeme.class);
+            vanillameme.putExtra("imgFilePath",imgFilePath);
+            startActivity(vanillameme);
+        }
+        else {
+            Intent demotivationalMeme = new Intent(ChooseMemeStyle.this, DemotivationalMemeActivity.class);
+            demotivationalMeme.putExtra("imgFilePath",imgFilePath);
+            startActivity(demotivationalMeme);
+        }
     }
+
+
 }
 
